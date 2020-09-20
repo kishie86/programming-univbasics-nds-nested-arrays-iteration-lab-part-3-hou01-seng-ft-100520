ae2f7ea49201958c361f779dbdc0d8139111d9d5
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 01:43:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,13 +3,13 @@ def join_nested_strings(src)
 
 results = []
 row_index = 0
-while row_index < join_nested_strings(src).count do
+while row_index < src.count do
   element_index = 0
   pull_strings_only = ""
   
   while element_index < join_nested_strings(src)[row_index].count do
-    if join_nested_strings(src)[row_index][element_index].length > pull_strings_only.length
-    pull_strings_only = join_nested_strings(src)[row_index][element_index]
+    if src[row_index][element_index].length > pull_strings_only.length
+    pull_strings_only = src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
