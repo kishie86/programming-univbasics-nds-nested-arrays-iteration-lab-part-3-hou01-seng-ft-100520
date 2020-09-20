2db231350e336208fda40a0e560cddb6065502f4
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 00:45:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ total = 0
 row_index = 0
 while row_index < join_nested_strings(src).count do
   element_index = 0
-  longest_string_element = ""
+  pull_strings_only = ""
   
   while element_index < join_nested_strings(src)[row_index].count do
     total += join_nested_strings(src)[row_index][element_index]
