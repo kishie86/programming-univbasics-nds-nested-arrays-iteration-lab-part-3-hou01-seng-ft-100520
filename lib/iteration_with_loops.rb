ec6946b554d17a1ec58d68e9f2e80610ce7e7b0e
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 02:04:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-    pull_strings_only += src[row_index][element_index] + " "
+    pull_strings_only += src[row_index][element_index] + ''
   
   end
   row_index += 1
