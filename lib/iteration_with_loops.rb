a6e2a6f266de6b4affcaa140c0cb2efc59624320
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 01:50:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,10 @@ def join_nested_strings(src)
 
 results = []
 row_index = 0
+
 while row_index < src.count do
   element_index = 0
-  pull_strings_only = ""
+
   
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
